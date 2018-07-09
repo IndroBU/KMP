a99d69602bc08c93627a3d6e95a2cf0433eecae3
Indrojit Mondal <indro.cse.bu@gmail.com>
2018-07-10 04:11:56 +0600

    Add files via upload

--- KMP.cpp
+++ KMP.cpp
@@ -1,64 +1,113 @@
+
+/**************************************************************************\
+*                 MAY GOD FORGIVE ME IF I DO SOMETHING WRONG                *
+*      __    ___       __   _________      ________         __________      *
+*     |  |  |   \     |  |  |        \     |       \        |  ____   |     *
+*     |  |  |    \    |  |  |   __    \    |  ___   \       |  |   |  |     *
+*     |  |  |  |  \   |  |  |  |   \   |   |  |  \   |      |  |   |  |     *
+*     |  |  |  |\  \  |  |  |  |    |  |   |  |__/  /       |  |   |  |     *
+*     |  |  |  | \  \ |  |  |  |___/   /   |  |   \ \       |  |   |  |     *
+*     |  |  |  |  \  \|  |  |         /    |  |    \ \___/  |  |___|  |     *
+*     |__|  |__|   \_____|  |________/     |__|     \___/   |_________|     *
+*                                                                           *
+*                    Computer Science and Engineering                       *
+*                        UNIVERSITY  OF BARISAL                             *
+*                                                                           *
+*              NOW LET'S TRY TO SOLVE  PROGRAMMING PROBLEM               *
+*                                                                           *
+****************************************************************************/
 #include<bits/stdc++.h>
 
 using namespace std;
-int b[1100000];
+char P[10000000];
+char T[10000000];
 
-string Str;
-string Substr;
-
-
-int loc;
-void kmpPreprocess(string Substr)
+long long int frequency=0,loc;
+void KMP(string T, string P)
 {
-    int m = Substr.size();
-    int i = 0, j = -1;
-
-    b[i] = -1;
-    while(i < m)
+       long long int n,m,k,i,j;
+   // n=T.size();
+    n=T.size();
+    //m=P.size();
+    m=P.size();
+    long long int  LPS[m];
+    i=1;
+    j=0;
+    LPS[0]=0;
+    while(i<m)
     {
-        while(j >= 0 && Substr[i] != Substr[j])
-            j = b[j];
 
-		i++;
-        j++;
-        b[i] = j;
+            if(P[i]==P[j]){
+                    j=j+1;
+              LPS[i]=j;
+              i=i+1;
+
+            }
+            else
+            {
+                if(j)
+                   j=LPS[j-1];
+            else {
+                    LPS[i]=0;
+                    i=i+1;
+            }
+            }
+    }
+   /*for(i=0; i<m; i++)
+    {
+       // cout<<A[i]<<" ";
+       printf("%lld ",LPS[i]);
+    }*/
+    //cout<<endl;
+    //printf("\n");
+    i=0;
+    j=0;
+
+    while(i<n)
+    {
+
+
+
+           if(T[i]==P[j]){
+
+              j=j+1;
+
+              i=i+1;
+            }
+            if(j==m)
+            {
+                frequency=frequency+1;
+                loc=i-j;
+
+                j=LPS[j-1];
+            }
+            else if(i<n && T[i]!=P[j])
+            {
+                if(j)
+                    j=LPS[j-1];
+                else
+                   i=i+1;
+
+            }
+
+
     }
 }
-bool KMPCk=false;
-void  kmp(string Str, string Substr)
-{
-    int i=0, j=0;
-     int n = Str.size();
-    int m = Substr.size();
-
-	kmpPreprocess(Substr);
-    while (i<n)
-    {
-        while (j>=0 && Str[i]!=Substr[j])
-            j=b[j];
-
-        i++;
-        j++;
-
-        if (j==m)
-        {
-
-            loc=i-j;
-            KMPCk=true;
-            j=b[j];
-        }
-    }
-
-}
-
-
-
 int main()
 {
+   // string P,T;
 
-   cin>>Str>>Substr;
-   kmp(Str,Substr);
-   cout<<loc<<endl; /// 0 based
-	return 0;
+    long long int cs,ct;
+
+   cin>>T>>P;
+
+
+
+     KMP(T,P);
+      printf("%lld\n",loc);
+      loc=0;
+
+
+
+    return 0;
 }
-
